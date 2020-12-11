fa796b269f01bdd5979238250cc55846cda7fc2e
Ken Dickey <seatacken@gmail.com>
2020-12-10 22:59:03 -0800

    Added click to move and camera follow player with main cam under follow cam obj with script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,9 +9,30 @@ public class Mover : MonoBehaviour
     [SerializeField] Transform target;
 
 
-    // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        //point main camera to mouse pos when left mouse is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
     }
-}
+
+    private void MoveToCursor()
+    {
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; //out stores info to hit variable
+        //create a racast that takes in a ray, an out, which outputs hit info, and a hit.. as a bool
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;//formerly target.position;
+        }
+    }
+
+
+        
+ }
+
+    
