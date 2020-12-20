0e48646c1facefb238f10113bb294076df869226
Ken Dickey <seatacken@gmail.com>
2020-12-20 15:31:18 -0800

    Start of enemy ai with chase distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f; //5 unity units
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance) // if xfloat < yfloat
+            {              
+                print(gameObject.name + "should chase");
+            }
+        }
+
+        private float DistanceToPlayer() //returns a float
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
