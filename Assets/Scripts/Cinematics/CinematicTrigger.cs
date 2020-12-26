78ba38c469222cc7e451f999533451a916755f6b
Ken Dickey <seatacken@gmail.com>
2020-12-26 15:32:46 -0800

    Trigger cinematic script

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables; //includes Playable Director in inspector
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alReadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alReadyTriggered && other.gameObject.tag == "Player")
+            {
+                alReadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+
+            }
+        }
+    }
+}
+
