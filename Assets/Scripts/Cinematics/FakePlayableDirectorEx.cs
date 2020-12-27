c40e50ab08ec4f9be615df3c6e9c58e6bddf5571
Ken Dickey <seatacken@gmail.com>
2020-12-27 02:13:46 -0800

    Playable director example

---
+++ Assets/Scripts/Cinematics/FakePlayableDirectorEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//example used in conjunction with CinematicsControlRemover, lecture 66
+namespace RPG.Cinematics
+{
+    public class FakePlayableDirector : MonoBehaviour
+    {
+        public event Action<float> onFinish; //var that represents a list of callbacks
+
+        void Start()
+        {
+            Invoke("OnFinish", 3f); //invoke this method after 3 seconds
+        }
+
+        void OnFinish()
+        {
+            onFinish(4.3f); ////4.3f is just a placeholder
+        }
+    }
+}
