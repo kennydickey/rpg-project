dfeb567579526d5a6b06bfee48426723094119ec
Ken Dickey <seatacken@gmail.com>
2020-12-27 01:53:48 -0800

    Added control remover for cinematic

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl; //notice no call parens
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+        void DisableControl(PlayableDirector playableDirector)
+        {
+            print("disablecontrol");
+        }
+        void EnableControl(PlayableDirector playableDirector)
+        {
+            print("enablecontrol");
+        }
+    }
+}
