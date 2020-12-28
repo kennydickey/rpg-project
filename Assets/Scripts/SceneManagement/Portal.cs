2d10437aaec287b3bd2662784791286d068b5801
Ken Dickey <seatacken@gmail.com>
2020-12-28 11:52:08 -0800

    Asynced portals

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -11,12 +11,23 @@ namespace RPG.SceneManagement
 
         private void OnTriggerEnter(Collider other) //any other collider
         {
-            print("portal trigger");
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                //SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
         
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject); // keeps portal gameObject, assuming portal is at root of scene
+            //IEnums require a yield return
+            //yield and call again when scene is finished loading
+            yield return SceneManager.LoadSceneAsync(sceneToLoad); //allows us to transfer info into new scene
+            //after scene load
+            print("scene loaded");
+            Destroy(gameObject); // this script is attached to portal, so will destroy portal
         }
     }
 }
