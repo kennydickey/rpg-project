82a4cb6a514b523309c20b8123496b1e7c15de0b
Ken Dickey <seatacken@gmail.com>
2020-12-28 17:11:42 -0800

    Contructed alpha fading coroutine in Fader.cs

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup; // located in fader canvas inspector
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            //canvasGroup.alpha = 
+            //StartCoroutine(FadeOut(3f));
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            print("faded out");
+            yield return FadeIn(1f);
+            print("faded in");
+        }
+
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) // alpha is not 1
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null; //with null, coroutine will run again on next possible frame
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) // alpha is not 1
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null; //with null, coroutine will run again on next possible frame
+            }
+        }
+    }
+
+}
+
