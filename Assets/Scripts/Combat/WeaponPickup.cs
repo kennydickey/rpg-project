f80cebec801cbd838cb88e338ccb98211dae425b
Ken Dickey <seatacken@gmail.com>
2021-01-11 20:47:20 -0800

    Pickup respawning

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,14 +9,35 @@ namespace RPG.Combat
     {
 
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 10;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject); // Gameobject gameObject of which this script is attached
+                //Destroy(gameObject); // Gameobject gameObject of which this script is attached
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow; // !only for the collider next to this script
+            //uncheck the actual weapon obj vv
+            //transform.GetChild(0).gameObject.SetActive(shouldShow); //or all childed objs..
+            foreach(Transform child in transform) // all objs in transform, child can be named anything
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
     }
 }
