e0edb68bdf776340fbb64b1734acdf41eda3cd1c
Ken Dickey <seatacken@gmail.com>
2021-01-15 19:49:50 -0800

    Higher performance lookup using nested dictionaries

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace RPG.Stats {
     [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
@@ -8,24 +10,60 @@ namespace RPG.Stats {
         //our actual field
         [SerializeField] ProgressionCharacterClass[] characterClasses = null; //null starts it as 0
 
+        //              key                val<key, val>
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStatProg(Stat stat, CharacterClass characterClass, int level)
         {
-            // iterate over characterClasses Array
-            foreach(ProgressionCharacterClass progressionClass in characterClasses)
-            {
-                if (progressionClass.characterClass != characterClass) continue; // next iteration
+            BuildLookup();
 
-                // iterate over serialized stats
-                foreach(ProgressionStat progressionStat in progressionClass.stats)
-                {
-                    if (progressionStat.stat != stat) continue;
-                    if (progressionStat.levels.Length < level) continue; // if lvl is not in array
-                    // otherwise..
-                    return progressionStat.levels[level - 1];
-                }
+            //return lookupTable[characterClass][stat][level]; //rather..
+            float[] levels = lookupTable[characterClass][stat];
+
+            if(levels.Length < level) //if level is higher than array
+            {
+                return 0;
             }
-            // otherwise default to vv
-            return 0;
+            //otherwise..
+            return levels[level - 1];
+
+            //// iterate over characterClasses Array
+            //foreach(ProgressionCharacterClass progressionClass in characterClasses)
+            //{
+            //    if (progressionClass.characterClass != characterClass) continue; // next iteration
+
+            //    // iterate over serialized stats
+            //    foreach(ProgressionStat progressionStat in progressionClass.stats)
+            //    {
+            //        if (progressionStat.stat != stat) continue;
+            //        if (progressionStat.levels.Length < level) continue; // if lvl is not in array
+            //        // otherwise..
+            //        return progressionStat.levels[level - 1];
+            //    }
+            //}
+            //// otherwise default to vv
+            //return 0;
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null) return; // if lookupTable was already built
+            //create outer Dictionary // () is (empty)
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+
+            foreach (ProgressionCharacterClass progressionClass in characterClasses)
+            {
+                var statLookupTable = new Dictionary<Stat, float[]>();
+                //populate statLookupTable by iterating to insert keys / vars
+                foreach (ProgressionStat progressionStat in progressionClass.stats)
+                {
+                    // inner Dictionary             key                     var
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
+                }
+                // outer dictionary          key               var
+                lookupTable[progressionClass.characterClass] = statLookupTable;
+            }
         }
 
 
