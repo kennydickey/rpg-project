6ec3239c1563779df2c8d956b50c898132a05bab
Ken Dickey <seatacken@gmail.com>
2021-01-17 14:26:49 -0800

    Level display

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+
+        private void Awake()
+        {
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        private void Update()
+        {
+            // update text with format - : 0.0 is decimal places
+            GetComponent<Text>().text = string.Format("{0:0}", baseStats.GetLevel());
+        }
+
+    }
+}
