df4229197cc9be170fe4b2cc3483ae448ab2ea1b
Ken Dickey <seatacken@gmail.com>
2021-02-01 12:06:42 -0800

    Start making unique identifiers

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Saving
-{ 
+{
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        //simple uid, but unreliable and we need for each character
+        //[SerializeField] string uniqueIdentifier = System.Guid.NewGuid().ToString();
+        [SerializeField] string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier()
         {
             return "" + " got from getuniqueidentifier";
@@ -21,6 +26,12 @@ namespace RPG.Saving
         {
             print("restoring state for " + GetUniqueIdentifier());
 
+        }
+
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return; //
+            print("editing");
         }
 
     }
