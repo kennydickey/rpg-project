56eb3770e8b0822e31a2dff455835bfcaf1c1af9
Ken Dickey <seatacken@gmail.com>
2021-03-07 14:28:03 -0800

    Working health bar updating with damage

---
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Attributes
+{
+
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] Health healthComponent = null;
+        [SerializeField] RectTransform foreGround = null;
+
+        void Update()
+        {
+            //foreGround.Image.Color
+            //   = healthComponent.Healthpoints.value;
+            foreGround.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
+        }
+    }
+}
